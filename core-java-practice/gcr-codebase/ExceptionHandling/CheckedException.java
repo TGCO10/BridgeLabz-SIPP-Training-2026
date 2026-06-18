f9d45a49203f6ac38f488c20bb2e48c07714f9cb
TGCO10 <tarang4624@gmail.com>
2026-06-18 09:40:39 +0530

    [Tarang].add: /gcr-codebase/ExceptionHandling/

---
+++ core-java-practice/gcr-codebase/ExceptionHandling/CheckedException.java
@@ -0,0 +1,22 @@
+package gcrCodeBase.ExceptionHandling;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CheckedException extends Exception {
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("data.txt"));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            br.close();
+        } catch (IOException e) {
+            System.out.println("File not found");
+        }
+    }
+}
---
+++ core-java-practice/gcr-codebase/ExceptionHandling/ExceptionPropagationInMethods.java
@@ -0,0 +1,20 @@
+package gcrCodeBase.ExceptionHandling;
+
+public class ExceptionPropagationInMethods {
+
+    public static void method1() {
+        int result = 10 / 0; // ArithmeticException
+    }
+
+    public static void method2() {
+        method1();
+    }
+
+    public static void main(String[] args) {
+        try {
+            method2();
+        } catch (ArithmeticException e) {
+            System.out.println("Handled exception in main");
+        }
+    }
+}
