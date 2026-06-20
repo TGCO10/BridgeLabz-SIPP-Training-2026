587679ad8a9c5d794e39358e7b105c3efd8aa1b8
TGCO10 <tarang4624@gmail.com>
2026-06-20 16:33:34 +0530

    [Tarang].add: /gcr-codebase/ControlFlow/

---
+++ core-java-practice/gcr-codebase/ControlFlow/Level - 2/FactorsUsingWhile.java
@@ -0,0 +1,28 @@
+package gcrCodeBase.ControlFlow.level2;
+
+import java.util.Scanner;
+
+public class FactorsUsingWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+
+        if (number > 0) {
+            System.out.println("Factors of " + number + " are:");
+
+            int counter = 1;
+
+            while (counter < number) {
+                if (number % counter == 0) {
+                    System.out.println(counter);
+                }
+                counter++;
+            }
+        } else {
+            System.out.println("Please enter a positive integer.");
+        }
+
+        sc.close();
+    }
+}
---
+++ core-java-practice/gcr-codebase/ControlFlow/Level - 2/FriendsInfo.java
@@ -0,0 +1,40 @@
+package gcrCodeBase.ControlFlow.level2;
+
+import java.util.Scanner;
+
+public class FriendsInfo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int amarAge = sc.nextInt();
+        int akbarAge = sc.nextInt();
+        int anthonyAge = sc.nextInt();
+
+        double amarHeight = sc.nextDouble();
+        double akbarHeight = sc.nextDouble();
+        double anthonyHeight = sc.nextDouble();
+
+        String youngest;
+        if (amarAge < akbarAge && amarAge < anthonyAge) {
+            youngest = "Amar";
+        } else if (akbarAge < amarAge && akbarAge < anthonyAge) {
+            youngest = "Akbar";
+        } else {
+            youngest = "Anthony";
+        }
+
+        String tallest;
+        if (amarHeight > akbarHeight && amarHeight > anthonyHeight) {
+            tallest = "Amar";
+        } else if (akbarHeight > amarHeight && akbarHeight > anthonyHeight) {
+            tallest = "Akbar";
+        } else {
+            tallest = "Anthony";
+        }
+
+        System.out.println("Youngest Friend: " + youngest);
+        System.out.println("Tallest Friend: " + tallest);
+
+        sc.close();
+    }
+}
